1d4b1742df13c9b14d26d5dae13ee92f503f07cc
mohamed sadiq <mohammedsadeg@outlook.sa>
2017-05-01 17:14:48 +0200

    Add files via upload

---
+++ ntsc-1.1/en/contactthanks.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+		<h1>Your message has been sent!</h1><br />
+		<p><a href="index.html">Main Website</a></p>
+	</div>
+
+
+
+</body>
+
+</html>
---
+++ ntsc-1.1/js/main.js
@@ -0,0 +1,3 @@
+$(" .home").vegas({slides:[{src:"img/samuel-zeller-118195.jpg"},{src:"img/freestocks-org-126848.jpg"},{src:"img/daniel-frank-201417.jpg"},{src:"img/OGMQ8I0.jpg"}],});AOS.init({duration:1200,})
+$(document).ready(function(){$('#nav-icon4').click(function(){$(this).toggleClass('open');$(".nav").toggleClass("navi")
+$(".media-1 .icons").toggleClass("meni")})})
